05e5781f7fa220b2bbb9b783c98abefdbc9d5756
Sean Rowan <seanrowan@zipcodersMBP37.localdomain>
2019-04-30 17:11:23 -0400

    Initial Commit

---
+++ src/main/java/com/rowan/HSPOConnect/HspoConnectApplication.java
@@ -0,0 +1,13 @@
+package com.rowan.HSPOConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HspoConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HspoConnectApplication.class, args);
+	}
+
+}
